19b56f6a7d5b90556ee39a90e2b59fe116ab8e71
uday kumar <uday89090@gmail.com>
2024-06-23 08:09:32 +0530

    switch.js

---
+++ switch.js
@@ -0,0 +1,28 @@
+//                   switch case statement(day name) 1->monday  7->sunday  
+
+let day = 9;
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log(wednesday);
+        break;
+    case 4:
+        console.log("thrusday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+    default:
+        console.log("invaild number");
+}
