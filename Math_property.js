a6737a32a014d6b1cf1c54b6c2b7b76f9b861e0f
uday kumar <uday89090@gmail.com>
2024-06-23 07:57:15 +0530

    Math_property.js

---
+++ Math_property.js
@@ -0,0 +1,11 @@
+//    Math.PI
+console.log(Math.PI);
+
+//    Math.SQRT2 => It gives square root of 2
+console.log(Math.SQRT2);
+
+//    Math.LN2
+console.log(Math.LN2);
+
+//    Math.LN10
+console.log(Math.LN10);
