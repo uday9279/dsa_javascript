35bb2801449e5924f5a3601789c5a64fd2f3a7a6
uday kumar <uday89090@gmail.com>
2024-06-23 08:18:31 +0530

    date_method.js

---
+++ date_method.js
@@ -0,0 +1,40 @@
+//     How to create Date()
+let date=new Date()
+console.log(date);
+
+
+//    now()
+console.log(Date.now());
+
+
+//    getDate()
+console.log(date.getDate());
+
+
+//    getMonth()
+console.log(date.getMonth());  // month index starting with zero
+
+
+//    getDay()
+console.log(date.getDay());   // 1 index = 'monday'
+
+
+//    getFullYear()
+console.log(date.getFullYear());
+
+
+//    getHours()
+console.log(date.getHours());
+
+
+//    getMinutes()
+console.log(date.getMinutes());
+
+
+//    getTime()
+console.log(date.getTime());
+
+
+//    toDateString()
+console.log(date.toDateString());
+
