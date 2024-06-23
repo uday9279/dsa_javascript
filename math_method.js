7d7525e2497da1c13947e2ddb65e2e057407d642
uday kumar <uday89090@gmail.com>
2024-06-23 07:43:18 +0530

    math method

---
+++ math_method.js
@@ -0,0 +1,41 @@
+//   Math.abs()=> It work just like mod
+console.log(Math.abs(-16));
+
+//   Math.ceil()
+ console.log(Math.ceil(12.2));
+ console.log(Math.ceil(12.8));
+
+ //  Math.exp=> it gives e to the power where e = Euler's no (2.718)
+ console.log(Math.exp(2));
+
+ //      Math.trunc()
+ console.log(Math.trunc(14.45));
+ console.log(Math.trunc(4.7));
+
+ //      Math.sqrt()
+ console.log(Math.sqrt(4));
+
+ //      Math.round()
+ console.log(Math.round(12.8));
+ console.log(Math.round(12.3));
+
+ //      Math.pow(base,exponent)
+ console.log(Math.pow(5,3));
+
+ //      Math.random()
+ console.log(Math.random());
+
+ // generate 0 to 1 random number
+ c=Math.random()*10;
+ console.log(Math.trunc(c));
+
+ //     Math.min() => It return smallest number
+ console.log(Math.min(5,12,17,21,3));
+
+
+//      Math.max() => It return largest number
+console.log(Math.max(3,11,2,23,32));
+
+//      Math.floor()=> It work just like Math.trunc()
+console.log(Math.floor(15.7));
+console.log(Math.floor(15.1));
