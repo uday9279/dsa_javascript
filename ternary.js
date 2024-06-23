d9d0db3067188b351a367b2fe7e625b632cc2bd6
uday kumar <uday89090@gmail.com>
2024-06-23 08:10:38 +0530

    ternary.js

---
+++ ternary.js
@@ -0,0 +1,6 @@
+//                             ternary operators for voting criteria
+
+
+let age =9;
+(age>=18)?console.log("you are eligible for voting"):
+console.log("you are not eligible for voting");
